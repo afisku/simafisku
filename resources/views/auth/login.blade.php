53da61ef2c2a8d76dde7eb35799be3c2388f1c37
SMPIT CODER <smpitalfityan2020@gmail.com>
2023-03-04 13:41:09 +0700

    tampilan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
                 <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
                     <div class="p-4 m-3">
                         <img src="../assets/img/sdmuhbrosot.png" alt="logo" width="80" class="shadow-light rounded-circle mb-5 mt-2">
-                        <h5 class="text-dark font-weight-normal">Aplikasi <span class="font-weight-bold">Inventaris Barang Sekolah</span></h5>
+                        <h5 class="text-dark font-weight-normal">Aplikasi <span class="font-weight-bold">SISTEM INFORMASI SMPIT AFISKU</span></h5>
                         <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
                             @csrf
                             <div class="form-group">
